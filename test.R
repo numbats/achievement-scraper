1d1bf7dc2a12e7c85ef9252634c59c51117f8a5f
Parnika Khattri <pkha0009@student.monash.edu>
2024-08-13 18:16:26 +1000

    Restructing the package

--- test.R
+++
@@ -1,8 +0,0 @@
-# Test the search by ORCID ID 
-search_by_orcid("0000-0003-2531-9408")
-
-# Test the search by Google Scholar ID 
-search_by_gsid("BZ07eocAAAAJ")
-
-# Test the search by Name function
-search_by_name("Rob","Hyndman")
