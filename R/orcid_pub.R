26fbe4c2fd723f2fda88f9510ba23cf0e2d2387b
Parnika Khattri <pkha0009@student.monash.edu>
2024-08-22 13:11:53 +1000

    Adding orcid_id function

--- R/orcid_pub.R
+++ R/orcid_pub.R
@@ -1,21 +1,20 @@
-#' Get Publications from ORCID
-#'
-#' @param orcid_id ORCID ID
-#' @return A tibble of publications from ORCID
-#' @export
-get_publications_from_orcid <- function(orcid_id) {
-  # This function is a placeholder.
-  orcid_pubs <- tibble::tibble(
-    title = c("Sample Title 1", "Sample Title 2"),
-    DOI = c("10.1234/example1", "10.1234/example2"),
-    authors = c("Author One, Author Two", "Author Three, Author Four"),
-    publication_date = as.Date(c("2020-01-01", "2021-02-01")),
-    journal_name = c("Journal A", "Journal B"),
-    software_name = c(NA, "Software X"),
-    num_downloads = c(NA, 1000),
-    last_update_date = as.Date(c(NA, "2022-05-01")),
-    original_publish_date = as.Date(c(NA, "2021-01-01"))
-  )
+# Read the CSV file and extract ORCID IDs
+orcid_data <- readr::read_csv("data-raw/orcid_gsid.csv")
+orcid_ids <- orcid_data %>%
+  filter(!is.na(orcid_id))%>%
+  pull(orcid_id)
 
-  return(orcid_pubs)
+# Initialize an empty list to store publications
+all_pubs <- list()
+
+# Loop over each ORCID ID to fetch their works
+for (orcid_id in orcid_ids) {
+  pubs <- orcid_works(orcid_id)
+  all_pubs[[orcid_id]] <- pubs[[1]]$works
 }
+
+all_pubs <- bind_rows(all_pubs)
+
+# Print the publications for each ORCID ID
+print(all_pubs)
+
--- data-raw/orcid_gsid.R
+++ data-raw/orcid_gsid.R
@@ -1,4 +1,5 @@
-# Reading the data 
+# Reading the data
 orcid_gsid <- readr::read_csv("data-raw/orcid_gsid.csv")
 # Documenting the data
 usethis::use_data(orcid_gsid, overwrite = T)
+
