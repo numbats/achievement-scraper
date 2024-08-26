bfca417f9e0d556ba618d314ad89d92462325a7e
Parnika Khattri <pkha0009@student.monash.edu>
2024-08-26 17:41:59 +1000

    Updating Rorcid function

--- R/orcid_pub.R
+++ R/orcid_pub.R
@@ -1,24 +1,46 @@
-library(rorcid)
-library(tidyverse)
+#' get_publications_from_orcid
+#'
+#' @description
+#' The `get_publications_from_orcid` function retrieves publications for given ORCID IDs using the rorcid package.
+#' It fetches all works associated with each ORCID ID and combines them into a single data frame.
+#'
+#' @param orcid_ids A character vector of ORCID IDs for which publication information is requested.
+#'
+#' @return A data frame containing all publications for the specified ORCID IDs.
+#'
+#' @examples
+#' # Retrieve publications for specific ORCID IDs
+#' get_publications_from_orcid(c("0000-0002-1825-0097", "0000-0003-1825-0098"))
+#'
+#' @rdname get_publications_from_orcid
+#'
+#' @export
 
-fetch_orcid_publications <- function(orcid_ids) {
+library(rorcid)
+library(dplyr)
+
+get_publications_from_orcid <- function(orcid_ids) {
   # Initialize an empty list to store publications
   all_pubs <- list()
 
   # Loop over each ORCID ID to fetch their works
   for (orcid_id in orcid_ids) {
     pubs <- orcid_works(orcid_id)
-    all_pubs[[orcid_id]] <- pubs[[1]]$works
+    if (!is.null(pubs[[1]]$works)) {
+      all_pubs[[orcid_id]] <- pubs[[1]]$works %>%
+        dplyr::select(
+          title = `work-title`$title$value,
+          DOI = `work-external-identifiers`$`work-external-identifier`[[1]]$`work-external-identifier-id`$value,
+          authors = `work-contributors`$`contributor`[[1]]$`credit-name`$value,
+          publication_date = `publication-date`$year$value,
+          journal_name = `journal-title`$value
+        )
+    } else {
+      all_pubs[[orcid_id]] <- tibble()
+    }
   }
 
-  all_pubs <- bind_rows(all_pubs)
-
-  # Print the publications for each ORCID ID
-  print(all_pubs)
+  # Combine all publications into a single data frame
+  return(bind_rows(all_pubs, .id = "orcid_id"))
 }
-
-# To use the function, provide the ORCID IDs when calling it:
-# fetch_orcid_publications(orcid_ids)
-
-get_publications_from_orcid()
 
