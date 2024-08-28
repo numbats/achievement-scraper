6daa8c1630aba17a9e8ebf59b8879b9f971cc449
Parnika Khattri <pkha0009@student.monash.edu>
2024-08-28 13:27:04 +1000

    Updating the rorcid function

--- R/orcid_pub.R
+++ R/orcid_pub.R
@@ -8,39 +8,37 @@
 #'
 #' @return A data frame containing all publications for the specified ORCID IDs.
 #'
+#' @importFrom rorcid orcid_works
+#' @importFrom dplyr select bind_rows
+#' @importFrom tibble tibble
+#' @importFrom magrittr %>%
+#'
 #' @examples
 #' # Retrieve publications for specific ORCID IDs
-#' get_publications_from_orcid(c("0000-0002-1825-0097", "0000-0003-1825-0098"))
+#' get_publications_from_orcid(c("0000-000X-XXXX-XXXX", "0000-000X-XXXX-XXXX"))
 #'
-#' @rdname get_publications_from_orcid
-#'
+#' @name get_publications_from_orcid
 #' @export
 
-library(rorcid)
-library(dplyr)
-
 get_publications_from_orcid <- function(orcid_ids) {
-  # Initialize an empty list to store publications
   all_pubs <- list()
 
-  # Loop over each ORCID ID to fetch their works
   for (orcid_id in orcid_ids) {
-    pubs <- orcid_works(orcid_id)
+    pubs <- rorcid::orcid_works(orcid_id)
     if (!is.null(pubs[[1]]$works)) {
       all_pubs[[orcid_id]] <- pubs[[1]]$works %>%
         dplyr::select(
-          title = `work-title`$title$value,
-          DOI = `work-external-identifiers`$`work-external-identifier`[[1]]$`work-external-identifier-id`$value,
-          authors = `work-contributors`$`contributor`[[1]]$`credit-name`$value,
-          publication_date = `publication-date`$year$value,
-          journal_name = `journal-title`$value
+          title = .data[[ "work-title" ]][[ "title" ]][[ "value" ]],
+          DOI = .data[[ "work-external-identifiers" ]][[ "work-external-identifier" ]][[1]][[ "work-external-identifier-id" ]][[ "value" ]],
+          authors = .data[[ "work-contributors" ]][[ "contributor" ]][[1]][[ "credit-name" ]][[ "value" ]],
+          publication_date = .data[[ "publication-date" ]][[ "year" ]][[ "value" ]],
+          journal_name = .data[[ "journal-title" ]][[ "value" ]]
         )
     } else {
-      all_pubs[[orcid_id]] <- tibble()
+      all_pubs[[orcid_id]] <- tibble::tibble()
     }
   }
 
-  # Combine all publications into a single data frame
-  return(bind_rows(all_pubs, .id = "orcid_id"))
+  return(dplyr::bind_rows(all_pubs, .id = "orcid_id"))
 }
 
