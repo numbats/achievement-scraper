1893a276e795f12ba1d6b1e112aaa780bd737a1f
Parnika Khattri <pkha0009@student.monash.edu>
2024-09-11 07:50:01 +1000

    updating description

--- data-raw/orcid_gsid.R
+++ data-raw/orcid_gsid.R
@@ -1,13 +1,4 @@
 # Reading the data
 orcid_gsid <- readr::read_csv("data-raw/orcid_gsid.csv")
-<<<<<<< Updated upstream
 # Document the data
 usethis::use_data(orcid_gsid, overwrite = T)
-# Saving the data
-save(orcid_gsid, file = "data/orcid_gsid.rda")
-
-
-=======
-# Documenting the data
-usethis::use_data(orcid_gsid, overwrite = T)
->>>>>>> Stashed changes
