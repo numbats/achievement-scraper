311dc8e7e0ca2a15b28e267e41ad2c3f38f7ac29
Parnika Khattri <pkha0009@student.monash.edu>
2024-09-19 09:42:25 +1000

    Adding tests for gs_pubs

--- tests/testthat/test-gs_pub.R
+++ tests/testthat/test-gs_pub.R
@@ -6,16 +6,33 @@ test_that("get_publications_from_scholar returns a valid data frame", {
   expect_true(nrow(result) > 0)
 })
 
-test_that("get_publications_from_scholar handles empty input", {
-  scholar_id <- ""
-  result <- get_publications_from_scholar(scholar_id)
-  expect_s3_class(result, "data.frame")
-  expect_equal(nrow(result), 0)
+
+test_that("get_publications_from_scholar handles missing DOI, journal, or year columns", {
+  result <- get_publications_from_scholar("vamErfkAAAAJ")
+  expect_s3_class(result, "tbl_df")
+  expect_true(all(c("DOI", "journal_name", "publication_year") %in% colnames(result)))
+  expect_true(!is.null(result$DOI))
+  expect_true(!is.null(result$journal_name))
+  expect_true(!is.null(result$publication_year))
 })
 
-test_that("get_publications_from_scholar handles invalid Google Scholar ID", {
-  scholar_id <- "invalidID"
+
+test_that("get_publications_from_scholar handles empty input", {
+  scholar_id <- character(0)
   result <- get_publications_from_scholar(scholar_id)
   expect_s3_class(result, "data.frame")
   expect_equal(nrow(result), 0)
-})
+}) #not working
+
+test_that("get_publications_from_scholar returns no NA values in publication_year", {
+  result <- get_publications_from_scholar("vamErfkAAAAJ")
+  expect_true("publication_year" %in% colnames(result))
+  expect_false(any(is.na(result$publication_year)))
+}) #NA values in publication_year
+
+test_that("get_publications_from_scholar handles invalid Google Scholar ID", {
+  scholar_id <- "invalid_Scholar_ID"
+  expect_error(get_publications_from_scholar(scholar_id), "Invalid SCHOLAR ID")
+})#not working
+
+
--- tests/testthat/test-orcid_pub.R
+++ tests/testthat/test-orcid_pub.R
@@ -1,4 +1,4 @@
-test_that("get_publications_from_orcid returns a valid data frame and returns correct publication year", {
+test_that("get_publications_from_orcid returns a valid data frame", {
   orcid_ids <- c("0000-0002-2140-5352", "0000-0001-6515-827X")
   result <- get_publications_from_orcid(orcid_ids)
   expect_s3_class(result, "data.frame")
