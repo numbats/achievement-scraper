0f9c32275c1a178e8f4d14edc544a91936e94f5d
Parnika Khattri <pkha0009@student.monash.edu>
2024-09-19 00:16:04 +1000

    Added a unit test to check if orcid_pubs returns publication year correctly

--- tests/testthat/test-orcid_pub.R
+++ tests/testthat/test-orcid_pub.R
@@ -1,3 +1,12 @@
+test_that("get_publications_from_orcid returns a valid data frame and returns correct publication year", {
+  orcid_ids <- c("0000-0002-2140-5352", "0000-0001-6515-827X")
+  result <- get_publications_from_orcid(orcid_ids)
+  expect_s3_class(result, "data.frame")
+  expect_true(all(c("title", "DOI", "authors", "publication_year", "journal_name", "orcid_id") %in% colnames(result)))
+  expect_false(any(is.na(result$publication_year)))
+  expect_true(nrow(result) > 0)
+})
+
 test_that("get_publications_from_orcid handles empty input", {
   orcid_ids <- character(0)
   result <- get_publications_from_orcid(orcid_ids)
@@ -5,24 +14,14 @@ test_that("get_publications_from_orcid handles empty input", {
   expect_equal(nrow(result), 0)
 })
 
-test_that("get_publications_from_orcid returns a valid data frame", {
-  orcid_ids <- c("0000-0002-2140-5352", "0000-0001-6515-827X")
-  result <- get_publications_from_orcid(orcid_ids)
-  expect_s3_class(result, "data.frame")
-  expect_true(all(c("title", "DOI", "authors", "publication_year", "journal_name", "orcid_id") %in% colnames(result)))
-  expect_true(nrow(result) > 0)
-})
-
 test_that("get_publications_from_orcid works even when certain columns don't exist", {
   result <- get_publications_from_orcid("0000-0001-9379-0010")
-
   expect_true(nrow(result) > 0)
   expect_true(all(c("title", "DOI", "authors", "publication_year", "journal_name", "orcid_id") %in% colnames(result)))
 })
-#
 
 test_that("get_publications_from_orcid returns empty data frame for ORCID with no works", {
-  orcid_ids <- c("0009-0008-4231-8291") #parnika
+  orcid_ids <- c("0009-0008-4231-8291")
   result <- get_publications_from_orcid(orcid_ids)
   expect_s3_class(result, "data.frame")
   expect_equal(nrow(result), 0)
@@ -33,5 +32,6 @@ test_that("get_publications_from_orcid returns empty data frame for invalid ORCI
   result <- get_publications_from_orcid(orcid_ids)
   expect_s3_class(result, "data.frame")
   expect_equal(nrow(result), 0)
-})
-#check the year
+}) #not working
+
+
